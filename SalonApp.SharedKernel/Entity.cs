e5a89fdf46653ab566ad7fc11c4bc18a2259b68c
nkosanovicn <nkosanovicn@gmail.com>
2026-06-24 19:16:14 +0200

    feat: add SharedKernel base classes (Entity, IDomainEvent, IDomainEventHandler)

---
+++ SalonApp.SharedKernel/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonApp.SharedKernel
+{
+    internal class Entity
+    {
+        public int id { get; set; }
+
+        private List<IDomainEvent> _domainEvents = new ();
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomaintEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
---
+++ SalonApp.SharedKernel/IDomainEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonApp.SharedKernel
+{
+    internal interface IDomainEventHandler<in TEvent> where TEvent : IDomainEvent
+    {
+        Task HandleAsync(IDomainEvent domainEvent);
+    }
+}
