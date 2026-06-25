c226f0dbca78da7e30b33842c787a799336a13cb
krakic <kristina.rakic@pupin.rs>
2026-06-25 16:06:03 +0200

    feat: add Staff module with StaffMember, WorkingSchedule and StaffService

---
+++ SalonApp.Modules.Staff/Domain/StaffMember.cs
@@ -0,0 +1,20 @@
+﻿using SalonApp.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonApp.Modules.Staff.Domain
+{
+    public class StaffMember : Entity
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Specialty { get; set; } = string.Empty; 
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public List<WorkingSchedule> WorkingSchedules { get; set; } = new ();
+    }
+}
