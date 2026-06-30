8a9b652ff4c1cef421e2d37f46337531a8b44f0c
krakic <kristina.rakic@pupin.rs>
2026-06-30 17:18:24 +0200

    feat: add SQLite repository implementation as alternative to JSON

---
+++ src/SalonApp.Infrastructure/Persistence/Sqlite/SqliteAppointmentRepository.cs
@@ -0,0 +1,30 @@
+﻿using SalonApp.Modules.Appointments.Application;
+using SalonApp.Modules.Appointments.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonApp.Infrastructure.Persistence.Sqlite;
+
+public class SqliteAppointmentRepository : SqliteRepository<Appointment>, IAppointmentRepository
+{
+    public SqliteAppointmentRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Appointment>> GetByStaffAndDateAsync(int staffMemberId, DateTime date)
+    {
+        return await _dbSet
+            .Where(a => a.StaffMemberId == staffMemberId && a.DateTime.Date == date.Date)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Appointment>> GetByClientAsync(int clientId)
+    {
+        return await _dbSet
+            .Where(a => a.ClientId == clientId)
+            .ToListAsync();
+    }
+}
--- src/SalonApp.WPF/App.xaml.cs
+++ src/SalonApp.WPF/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using SalonApp.Infrastructure.Persistence.Sqlite;
 using SalonApp.Infratructure.DependencyInjection;
 using SalonApp.WPF.ViewModels;
 using System.Configuration;
@@ -24,6 +25,9 @@ namespace SalonApp.WPF
 
             services.AddSalonServices(dataDirectory);
 
+            // Za SQLite:
+            // services.AddSalonServicesWithSqlite(Path.Combine(dataDirectory, "salon.db"));
+
             //ViewModels
             services.AddTransient<ClientsViewModel>();
             services.AddTransient<ServicesViewModel>();
@@ -32,6 +36,12 @@ namespace SalonApp.WPF
             services.AddTransient<AppointmentsViewModel>();
 
             _serviceProvider = services.BuildServiceProvider();
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.EnsureCreated();
+            }
 
             var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
             mainWindow.Show();
